add415456f0563f0e20f98036fd05e8512d3c2b0
Ednilson_Monteiro <ednilsonm.profissional@gmail.com>
2023-04-10 10:33:15 -0300

    Corrigir branch source

--- ethereum/factory.js
+++
@@ -1,9 +0,0 @@
-import web3 from "./web3";
-import CampaignFactory from "./build/CampaignFactory.json";
- 
-const instance = new web3.eth.Contract(
-  CampaignFactory.abi,
-  "0xfdA38E2971e90ebC35F39958BCa5603dC27ab795"
-);
- 
-export default instance;
